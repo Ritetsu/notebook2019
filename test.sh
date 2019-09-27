6e612be37fcd59409c46e855bad30ac9ac0b2ba0
linux_monika <1149875285@qq.com>
2019-09-27 14:40:36 +0800

    regular update

---
+++ test.sh
@@ -0,0 +1,8 @@
+for i in 'Python' 'Linux_LearningNotes' 'Course_C_LanguageProgramming' 'Course_R_LanguageProgramming'
+do
+  cd /home/sayori/git_local/$i
+  git add .
+  git commit -m "regular update"
+  git push
+  echo 'lgitAll2b9s...'
+done
