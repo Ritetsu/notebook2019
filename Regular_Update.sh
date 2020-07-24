b43e4403afd1447b7aeafdea82cc4e226896dd59
lizhe <lizhe@picb.ac.cn>
2020-07-24 15:33:41 +0800

    merged unnecessary repo

--- Regular_Update.sh
+++
@@ -1,7 +0,0 @@
-for i in 'Python' 'Linux_LearningNotes' 'Course_Materials' 'lizhe_Notes'
-do
-  cd /home/sayori/git_local/$i
-  git add -A
-  git commit -m "regular update"
-  git push origin master
-done
